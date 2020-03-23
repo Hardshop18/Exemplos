ef75bc44deef9344a32e26b828e7af7991fb3dbb
Hardshop18 <60898897+Hardshop18@users.noreply.github.com>
2020-03-23 14:19:42 -0300

    ticket 59902
    Criado exemplo vfpEfCore

---
+++ VfpEfCoreDemo/VfpEfCoreDemo/VfpDBContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using VfpEntityFrameworkProvider;
+
+namespace VfpEfCoreDemo
+{
+    [DbConfigurationType(typeof(VfpDbConfiguration))]
+    public class VfpDBContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        /*protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            string path = System.Environment.CurrentDirectory.ToString() + "\\..\\..\\..\\";
+            options.UseSqlite($"Data Source={path}sqlitedemo.db");
+        } */
+        public VfpDBContext()
+            : base(new VfpConnection(@"D:\GitHub\dados\SincaTeste.dbc"), true)
+        {
+        }
+
+        public VfpDBContext(string cs)
+            : base(new VfpConnection(cs), true)
+        {
+        }
+    }
+}
