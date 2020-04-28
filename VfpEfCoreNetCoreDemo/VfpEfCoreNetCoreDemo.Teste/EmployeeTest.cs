eee0e7add5eca427d6839a0639fbe4600510c1d4
desenv2 <desenv2@hardshop.com.br>
2020-04-28 17:49:25 -0300

    Ticket 61239
    Criado o projeto VfpEfCoreNetCoreDemo usando o https://github.com/tombrothers/EntityFrameworkCore.Vfp
    Renomear o projeto VfpEfCoreDemo para VfpEf6NetCoreDemo

---
+++ VfpEfCoreNetCoreDemo/VfpEfCoreNetCoreDemo.Teste/EmployeeTest.cs
@@ -0,0 +1,97 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace VfpEfCoreNetCoreDemo.Teste
+{
+    [TestClass]
+    public class EmployeeTest
+    {
+        VfpDBContext db;
+        public EmployeeTest()
+        {
+            string path = System.Environment.CurrentDirectory.ToString();
+            db = new VfpDBContext($"{path}\\Sinca.dbc");
+        }
+
+        private void CreateEmployee(string Name)
+        {
+            // Create
+            db.Employees.Add(new Employee { FirstName = Name, LastName = "Doe", Age = 55 });
+            db.SaveChanges(true);
+        }
+
+        private Employee GetEmployee()
+        {
+            try
+            {
+                return db.Employees
+                    .OrderBy(b => b.Id)
+                    .First();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void DeleteEmployee(Employee employee)
+        {
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+        }
+
+        [TestMethod]
+        //[Fact]
+        public void Create()
+        {
+            string Name = "John";
+            CreateEmployee(Name);
+
+            // Read
+            var employee = GetEmployee();
+
+            Assert.AreEqual(employee.FirstName, Name);
+
+            DeleteEmployee(employee);
+        }
+
+        [TestMethod]
+        //[Fact]
+        public void Update()
+        {
+            string Name = "Louis";
+            CreateEmployee("John");
+
+            // Read
+            var employee = GetEmployee();
+
+            // Update
+            employee.FirstName = Name;
+            employee.Age = 90;
+            db.SaveChanges();
+
+            // Read
+            employee = GetEmployee();
+
+            Assert.AreEqual(employee.FirstName, Name);
+
+            // Delete
+            DeleteEmployee(employee);
+        }
+        
+        [TestMethod]
+        //[Fact]
+        public void Delete()
+        {
+            // Create
+            CreateEmployee("John");
+            // Read
+            var employee = GetEmployee();
+            // Delete
+            DeleteEmployee(employee);
+
+            employee = GetEmployee();
+            Assert.IsNull(employee);
+        }
+    }
+}
