cb6d36b7e0bc0add8fe1694ce76e20447f40a73c
desenv2 <desenv2@hardshop.com.br>
2020-05-13 15:44:14 -0300

    Colocado o teste com o MySql
    Colocado o teste com o MySql

---
+++ Teste.MySql/Teste.MySql/Form1.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teste.MySql
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder stringBuilder = new MySqlConnectionStringBuilder();
+            stringBuilder.Server = "hardshop.ddns.net";
+            stringBuilder.Database = "sinca_vitoria";
+            stringBuilder.UserID = "sinca_vitoria";
+            stringBuilder.Password = "yUCKQn8T";
+            stringBuilder.Port = 3306;
+
+            stringBuilder.ConvertZeroDateTime = true;
+            stringBuilder.UseAffectedRows = false;
+            stringBuilder.SslMode = MySqlSslMode.None;
+
+            MySqlConnection mySql = new MySqlConnection(stringBuilder.ConnectionString);
+            try
+            {
+                mySql.Open();
+            }
+            catch(Exception erro)
+            {
+                MessageBox.Show(erro.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
